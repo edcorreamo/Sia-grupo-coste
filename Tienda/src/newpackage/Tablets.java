b5bf1690fc016939b5ea8045273e23abd3daac14
Prometeo <Prometeo@LAPTOP-38R5F0MR>
2018-09-26 16:35:52 -0500

    emmanuel

---
+++ Tienda/src/newpackage/Tablets.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author Prometeo
+ */
+public class Tablets {
+    String pantalla;
+    String espacialesParaLectura;
+    String referencia;
+    
+}
