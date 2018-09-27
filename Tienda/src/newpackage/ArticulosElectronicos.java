9503ba6415c9d93c5e9a8ebe5b3e618374b99cf5
Prometeo <Prometeo@LAPTOP-38R5F0MR>
2018-09-26 22:14:51 -0500

    emmanuel

--- Tienda/src/newpackage/ArticulosElectronicos.java
+++ Tienda/src/newpackage/ArticulosElectronicos.java
@@ -11,7 +11,46 @@ package newpackage;
  */
 public class ArticulosElectronicos {
    
-    String marca;
-    float preico;
-    String color;
+    private String marca;
+    private float precio;
+    private String color;
+    
+    public ArticulosElectronicos(String color,String marca,float precio){
+    setPrecio(precio);
+    setMarca(marca);
+    setColor(color);
+    }
+    public ArticulosElectronicos(){
+    setPrecio(-1);
+    setMarca("artuculo sin asignacion");
+    setColor("campo no agregado");
+    }
+    
+    
+    public void setMarca(String marca){
+    this.marca=marca;
+     }
+    public void setPrecio(float precio ){
+    this.precio=precio;
+    }
+    public void setColor(String color){
+    this.color=color;
+    }
+    public String getMarca(){
+    return this.marca;
+    }
+    public float getPrecio(){
+    return this.precio;
+    }
+    public String getColor(){
+    return this.color;
+    }
+    @Override
+    public String toString(){
+        String cadena="marca: "+getMarca()+"precio: "+getPrecio()+"color: "+getColor();
+      return cadena; 
 }
+  
+   }
+    
+    
